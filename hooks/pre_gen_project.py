17665aeec0c09723e4de3f6ac3dcc00dcc36549a
Kev <kev162020@gmail.com>
2023-02-24 14:38:32 -0600

    V3

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,13 +1,13 @@
 import subprocess
 
 #Entorno Virtual
-print("\033[1;34;46m"+"Creando entorno virtual..."+"\033[0;m")
+print("\033[1;34m"+"Creando entorno virtual..."+"\033[0;m")
 subprocess.run(['py','-m','venv','venv'])
-print("\033[1;34;46m"+"Entorno virtual creado"+"\033[0;m")
+print("\033[1;34m"+"Entorno virtual creado"+"\033[0;m")
 
 #Inicio repositorio
-print("\033[1;34;46m"+"Iniciando repositorio..."+"\033[0;m")
+print("\033[1;34m"+"Iniciando repositorio..."+"\033[0;m")
 subprocess.run(['git','init'])
-print("\033[1;34;46m"+"Repositorio inciado"+"\033[0;m")
+print("\033[1;34m"+"Repositorio inciado"+"\033[0;m")
 
-print("\033[1;34;46m"+"Proyecto configurado"+"\033[0;m")
+print("\033[1;34m"+"Proyecto configurado"+"\033[0;m")
