e35b7c09e110cfff407abed955f1dc07fab3d14f
Kev <kev162020@gmail.com>
2023-02-26 14:35:01 -0600

    V4

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,9 +1,11 @@
 import subprocess
+import time
 
 #Entorno Virtual
 print("\033[1;34m"+"Creando entorno virtual..."+"\033[0;m")
 subprocess.run(['py','-m','venv','venv'])
 print("\033[1;34m"+"Entorno virtual creado"+"\033[0;m")
+time.sleep(4)
 
 #Inicio repositorio
 print("\033[1;34m"+"Iniciando repositorio..."+"\033[0;m")
